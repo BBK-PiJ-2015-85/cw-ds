050281e66f5a538720ab1db84fa06f3d846b38bf
James Pickles <picklesj85@gmail.com>
2015-12-14 19:55:26 +0000

    removed erroneous statement

--- LinkedListTest.java
+++ LinkedListTest.java
@@ -2,21 +2,49 @@ public class LinkedListTest {
 	public static void main(String[] args) {
 		LinkedList ll = new LinkedList();
 		System.out.println(ll.isEmpty());
-		for (int i = 0; i < 1000000; i++) {
+		for (int i = 0; i < 10; i++) {
 			ll.add(i);
 		}
 		System.out.println(ll.size());
-		//ll.printList();
-		ll.add("x");
-		//ll.printList();
-		ll.remove(1000000);
+		ll.printList();
 		
-		ReturnObject ro = ll.get(1000000);
+		//ll.printList();
+		ReturnObject ro = ll.get(0);
 		if (ro.hasError()) {
 			System.out.println(ro.getError());
 		} else {
 			System.out.println(ro.getReturnValue());
 		}
+		ll.remove(1);
+		ll.printList();
+		ll.remove(0);
+		ll.printList();
+		ll.remove(7);
+		ll.printList();
+		ll.remove(0);
+		ll.printList();
+		ll.remove(4);
+		ll.printList();
+		ll.remove(3);
+		ll.printList();
+		ll.remove(2);
+		ll.printList();
+		ll.remove(0);
+		ll.printList();
+		ll.remove(1);
+		ll.printList();
+		ll.remove(0);
+		ll.printList();
+		System.out.println(ll.isEmpty());
+		System.out.println(ll.size());
+		
+		ro = ll.get(0);
+		if (ro.hasError()) {
+			System.out.println(ro.getError());
+		} else {
+			System.out.println(ro.getReturnValue());
+		}
+		/**
 		ro = ll.get(999999);
 		if (ro.hasError()) {
 			System.out.println(ro.getError());
@@ -29,7 +57,7 @@ public class LinkedListTest {
 		} else {
 			System.out.println(ro.getReturnValue());
 		}
-		
+		*/
 		
 	}
 }
