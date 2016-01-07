35f883674309707bef4a47c937ee71a34624ddd1
BBK-PiJ-2015-85 <picklesj85@gmail.com>
2016-01-07 15:30:32 +0000

    finished testing

--- StackImplTest.java
+++ StackImplTest.java
@@ -28,5 +28,78 @@ public class StackImplTest {
 		System.out.println();
 		System.out.println(si.toString());
 		
+		si.push(null);
+		System.out.println(si.toString());
+		System.out.println(si.isEmpty());
+		si.push("x");
+		ro = si.top();
+		System.out.println(ro.hasError());
+		System.out.println(ro.getError());
+		System.out.println(ro.getReturnValue());
+		si.pop();
+		si.pop();
+		si.pop();
+		si.pop();
+		System.out.println(si.toString());
+		
+		for (int i = 0; i < 1000000; i++) {
+			si.push(i);
+		}
+		System.out.println(si.size());
+		
+		for (int i = 1000006; i >= 0; i--) {
+			si.pop();
+		}
+		System.out.println(si.size());
+		System.out.println("************************************");
+		List ll = new LinkedList();
+		si = new StackImpl(ll);
+		
+		for (int i = 0; i < 10; i++) {
+			si.push(i);
+		}
+		
+		System.out.println(si.isEmpty());
+		System.out.println(si.size());
+		System.out.println(si.toString());
+		
+		System.out.println();
+		ro = si.top();
+		System.out.println(ro.hasError());
+		System.out.println(ro.getError());
+		System.out.println(ro.getReturnValue());
+		
+		System.out.println();
+		ro = si.pop();
+		System.out.println(ro.hasError());
+		System.out.println(ro.getError());
+		System.out.println(ro.getReturnValue());
+		
+		System.out.println();
+		System.out.println(si.toString());
+		
+		si.push(null);
+		System.out.println(si.toString());
+		System.out.println(si.isEmpty());
+		si.push("x");
+		ro = si.top();
+		System.out.println(ro.hasError());
+		System.out.println(ro.getError());
+		System.out.println(ro.getReturnValue());
+		si.pop();
+		si.pop();
+		si.pop();
+		si.pop();
+		System.out.println(si.toString());
+		
+		for (int i = 0; i < 1000000; i++) {
+			si.push(i);
+		}
+		System.out.println(si.size());
+		
+		for (int i = 1000; i >= 0; i--) {
+			si.pop();
+		}
+		System.out.println(si.size());
 	}
 }
